126188bd0f92e177b4600d5cf0909c574dd4dce1
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2024-04-22 01:24:35 -0400

    lil fix

--- src/routes/discography/+page.svelte
+++ src/routes/discography/+page.svelte
@@ -1,21 +1,27 @@
 <script lang="ts">
-import imageUrlBuilder from '@sanity/image-url'
-import { createClient } from "@sanity/client";
+	import imageUrlBuilder from '@sanity/image-url';
+	import { createClient } from '@sanity/client';
 
-export const client = createClient({
-	projectId: "1chuxo97",
-  	dataset: "production",
-  	apiVersion: "2021-10-21",
-  	useCdn: false,
-})
+	export const client = createClient({
+		projectId: '1chuxo97',
+		dataset: 'production',
+		apiVersion: '2021-10-21',
+		useCdn: false
+	});
 
-export let data;
+	export let data;
 
-const builder = imageUrlBuilder(client)
+	const builder = imageUrlBuilder(client);
 
-function urlFor(source: object): string {
-	return builder.image(source).toString()
-}
+	function urlFor(source: object) {
+		return builder.image(source);
+	}
+
+	function orderReleases(a: any, b: any) {
+		if (new Date(a.releaseDate) > new Date(b.releaseDate)) return -1;
+		if (new Date(a.releaseDate) < new Date(b.releaseDate)) return 1;
+		return 0;
+	}
 </script>
 
 <svelte:head>
@@ -26,14 +32,21 @@ function urlFor(source: object): string {
 	<h1 class="mb-10 text-2xl">Discography</h1>
 	<h2 class="mb-4 text-xl">Singles</h2>
 	{#if data.song && data.song.length}
-	<div class="grid grid-cols-5 gap-4">
-		{#each data.song as single}
-		<div class="min-w-28 mb-4">
-			<img src={urlFor(single.cover)} alt="Single Cover, temp description" class="mb-2"/>
-			<p>{single.title}</p>
-			<p class="text-xs">{new Date(single.releaseDate).getFullYear()}</p>
+		<div class="grid md:grid-cols-5 gap-4">
+			{#each data.song.sort(orderReleases) as single}
+				<div class="min-w-28 mb-4">
+					<img
+						src={urlFor(single.cover).width(600).toString()}
+						alt="Single Cover, temp description"
+						class="mb-2"
+						loading="lazy"
+					/>
+					<p>{single.title}</p>
+					<p class="text-xs">{new Date(single.releaseDate).getFullYear()}</p>
+				</div>
+			{/each}
 		</div>
-		{/each}
-	</div>
+	{:else}
+		<p>Loading...</p>
 	{/if}
 </div>
